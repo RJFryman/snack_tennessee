9d39af737d75cf1abeee370a751866ec56cf3c61
Robert Fryman <robert.fryman@gmail.com>
2015-05-02 09:10:27 -0500

    Add Begining fields to some migrations and ran rake db:migrate

--- db/migrate/20150502134307_create_makers.rb
+++ db/migrate/20150502134307_create_makers.rb
@@ -2,7 +2,15 @@ class CreateMakers < ActiveRecord::Migration
   def change
     create_table :makers do |t|
       t.string :name
-
+      t.string :website
+      t.string :address
+      t.string :state
+      t.integer :zip
+      t.string :city 
+      t.text :description
+      t.string :facebook
+      t.string :twitter
+      t.string :plus
     end
   end
 end
--- db/migrate/20150502134355_create_locations.rb
+++ db/migrate/20150502134355_create_locations.rb
@@ -2,6 +2,16 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.string :name
+      t.string :type
+      t.string :website
+      t.string :address
+      t.string :state
+      t.integer :zip
+      t.string :city
+      t.text :description
+      t.string :facebook
+      t.string :twitter
+      t.string :plus
     end
   end
 end
