f7a36c1a97b1c6274515eb458a75b506695ec56d
Robert Fryman <robert.fryman@gmail.com>
2015-05-02 12:16:21 -0500

    Get rails admin working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/data', as: 'rails_admin'
   devise_for :admins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
