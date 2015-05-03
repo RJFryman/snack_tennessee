716dbacc656866a66d8fb6d470db26001292b8dc
Alex <astory11@yahoo.com>
2015-05-03 13:02:22 -0500

    Removed Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
-
-groups = Group.create([{ :name => "JackDaniels", :imageurl => "http://www.jackdaniels.com/images/jack-logo-open-graph.png"}, { :name => "MoonPieCo", :imageurl => "https://pbs.twimg.com/profile_images/418783559751839744/R-RIDHrc.jpeg"}, { :name => "MountainDewIsntFromTN", :imageurl => "http://image-cdn.zap2it.com/images/mountain_dew-400-400.jpg"}])
